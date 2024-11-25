42d087e6bf5c2fe6a9f660e5b578f0b5e0e8b44f
ChenHY1217 <149823092+ChenHY1217@users.noreply.github.com>
2024-11-24 23:44:45 -0500

    Update API base URL for consistency and improved routing

--- src/frontend/src/app/constants.ts
+++ src/frontend/src/app/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://api.soraai.onrender.com";
+export const BASE_URL = "https://soraai-api.onrender.com";
 // export const BASE_URL = 'http://localhost:5000';
 export const USERS_URL = "/api/users";
 export const CHATS_URL = "/api/chats";
